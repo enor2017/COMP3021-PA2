12158e76f63e2f9971b45d33ab7bf9a019cbc21d
enor2017 <enor2017@163.com>
2021-11-02 01:14:28 +0800

    Done with UIServices.

--- app/src/main/java/hk/ust/cse/comp3021/pa2/view/UIServices.java
+++ app/src/main/java/hk/ust/cse/comp3021/pa2/view/UIServices.java
@@ -5,6 +5,7 @@ import hk.ust.cse.comp3021.pa2.model.GameState;
 import hk.ust.cse.comp3021.pa2.util.GameStateSerializer;
 import hk.ust.cse.comp3021.pa2.util.NotImplementedException;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.stage.FileChooser;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -26,9 +27,12 @@ public class UIServices {
      */
     @NotNull
     public static FileChooser createGameLoadFileChooser() {
-        // TODO: Create a file chooser with the provided string above \
+        // Create a file chooser with the provided string above \
         //  and set its initial directory to be the current working directory.
-        throw new NotImplementedException();
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle(FILE_CHOOSER_TITLE);
+        fileChooser.setInitialDirectory(getWorkingDirectory().toFile());
+        return fileChooser;
     }
 
     /**
@@ -74,8 +78,11 @@ public class UIServices {
      * Shows an {@link Alert} telling that there is an error when trying to open the game file.
      */
     public static void showLoadGameErrorDialog() {
-        // TODO: Show a dialog using the provided strings above.
-        throw new NotImplementedException();
+        // Show a dialog using the provided strings above.
+        Alert alert = new Alert(AlertType.ERROR);
+        alert.setTitle(LOAD_GAME_ERROR_ALERT_TITLE);
+        alert.setContentText(LOAD_GAME_ERROR_ALERT_CONTENT_TEXT);
+        alert.show();
     }
 
     private static final String LOAD_GAME_CANCEL_ALERT_TITLE = "Operation Cancelled";
@@ -86,8 +93,11 @@ public class UIServices {
      * Shows an {@link Alert} telling that the user has cancelled loading a game file.
      */
     public static void showFileNotSelectedDialog() {
-        // TODO: Show a dialog using the provided strings above.
-        throw new NotImplementedException();
+        // Show a dialog using the provided strings above.
+        Alert alert = new Alert(AlertType.ERROR);
+        alert.setTitle(LOAD_GAME_CANCEL_ALERT_TITLE);
+        alert.setContentText(LOAD_GAME_CANCEL_ALERT_CONTENT_TEXT);
+        alert.show();
     }
 
     private static final String WIN_ALERT_TITLE = "Congratulations";
@@ -98,8 +108,10 @@ public class UIServices {
      * Shows an {@link Alert} telling that the user has won the game.
      */
     public static void showWinDialog() {
-        // TODO: Show a dialog using the provided strings above.
-        throw new NotImplementedException();
+        Alert alert = new Alert(AlertType.INFORMATION);
+        alert.setTitle(WIN_ALERT_TITLE);
+        alert.setContentText(WIN_ALERT_CONTENT_TEXT);
+        alert.show();
     }
 
     private static final String LOSE_ALERT_TITLE = "Oops";
@@ -110,7 +122,10 @@ public class UIServices {
      * Shows an {@link Alert} telling that the user has lost the game.
      */
     public static void showLoseDialog() {
-        // TODO: Show a dialog using the provided strings above.
-        throw new NotImplementedException();
+        // Show a dialog using the provided strings above.
+        Alert alert = new Alert(AlertType.INFORMATION);
+        alert.setTitle(LOSE_ALERT_TITLE);
+        alert.setContentText(LOSE_ALERT_CONTENT_TEXT);
+        alert.show();
     }
 }
